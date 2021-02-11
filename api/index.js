587e6a27942a03f162bbcc3a2afd3d29298ac6c9
Vithikamandhwani <76060403+Vithikamandhwani@users.noreply.github.com>
2021-02-12 01:40:52 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { createServer } = require("http");
 // 👉 Replace this with express-oauth2-bearer require 👈
-const { auth } = require("express-oauth2-bearer");
+const { auth, requiredScopes } = require("express-oauth2-bearer");
 const {
   checkUrl,
   APP_URL, // Public URL for this app
@@ -52,7 +52,7 @@ app.get("/total", (req, res) => {
 // 👆 public routes above 👆
 app.use(auth());
 // 👇 private routes below 👇
-app.get("/reports", (req, res) => {
+app.get("/reports", requiredScopes('read:reports'),(req, res) => {
   res.send(expenses);
 });
 
